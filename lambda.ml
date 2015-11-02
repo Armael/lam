4f26955348dc79fc32cfc8deacc00fec0d9a49a2
Armael <armael.gueneau@ens-lyon.fr>
2015-11-02 15:25:54 +0100

    Attempt a CPS transform for Perform/Handle using two iterated cps
    This does not work: if we pass at the toplevel only two continuations,
    arguments are missing when calling print; but if we pass four, at some
    point there is too many arguments for a call to print.

--- lambda.ml
+++ lambda.ml
@@ -23,67 +23,15 @@ and t =
 | App of t * t
 | Atom of atom
 | Prim of int * (value list -> t)
+| Perform of t
+| Handle of t * t
 
 and value = Closure of value Ident.Map.t * t
 
-let rec subst map e =
-  match e with
-  | Var x -> (try Ident.Map.find x map with Not_found -> e)
-  | Lambda (x, e) -> Lambda (x, subst map e)
-  | App (u, v) -> App (subst map u, subst map v)
-  | _ -> e
-
-let rec cps e =
-  let k = Ident.create "k" in
-  let cont e c =
-    let is_value = function
-      | Var _ | Atom _ | Prim _ -> true
-      | _ -> false in
-
-    match e with
-    | Lambda (k, App (Var k', e')) when k = k' && is_value e' ->
-      begin match c with
-        | Var k ->
-          App (Var k, e')
-        | Lambda (kx, kbody) ->
-          subst Ident.Map.(add kx e' empty) kbody
-        | _ -> raise (Invalid_argument "cont")
-      end
-
-    | _ ->
-      App (e, c)
-  in
-
-  match e with
-  | Var _ | Atom _ ->
-    Lambda (k, App (Var k, e))
-  | Prim (n, p) ->
-    let p' =
-      Prim (n + 1, fun l ->
-        let l = List.rev l in
-        let Closure (_, k) = List.hd l in
-        let args = List.rev @@ List.tl l in
-        App (k, p args)) in
-    Lambda (k, App (Var k, p'))
-
-  | Lambda (x, e) ->
-    Lambda (k, App (Var k, Lambda (x, cps e)))
-  | App (u, v) ->
-    let val_u = Ident.create "v" in
-    let val_v = Ident.create "v" in
-    Lambda (k,
-      cont (cps u) (Lambda (val_u,
-        cont (cps v) (Lambda (val_v,
-          cont (App (Var val_u, Var val_v)) (Var k))))))
-
-let cps_main e =
-  let x = Ident.create "x" in
-  App (cps e, Lambda (x, Var x))
-
-(* Useful operators *)
+(* Helpers ********)
 
 let lam idents body =
-  List.fold_left (fun acc id -> Lambda (id, acc)) body idents
+  List.fold_right (fun id acc -> Lambda (id, acc)) idents body
 
 let app f args =
   let hd, args = (List.hd args), (List.tl args) in
@@ -132,6 +80,15 @@ let print_t cout =
       if paren then output_string cout ")"
     | Atom a -> print_atom cout a
     | Prim (n, _) -> Printf.fprintf cout "<prim %d>" n
+    | Perform e ->
+      if paren then output_string cout "(";
+      output_string cout "perform "; aux false e;
+      if paren then output_string cout ")"
+    | Handle (e, h) ->
+      if paren then output_string cout "(";
+      output_string cout "handle "; aux false e;
+      output_string cout " with "; aux false h;
+      if paren then output_string cout ")"
 
   and aux_lambda idents = function
     | Lambda (i, e) -> aux_lambda (i :: idents) e
@@ -149,14 +106,101 @@ let print_t cout =
   in
   aux false
 
-(* Interpreter *)
+(* CPS transform *******)
 
-exception Segfault
+let rec subst map e =
+  match e with
+  | Var x -> (try Ident.Map.find x map with Not_found -> e)
+  | Lambda (x, e) -> Lambda (x, subst map e)
+  | App (u, v) -> App (subst map u, subst map v)
+  | _ -> e
+
+let rec cps e =
+  let k = Ident.create "k" in
+  let kf = Ident.create "kf" in
+  let cont e c cf =
+    let is_value = function
+      | Var _ | Atom _ | Prim _ -> true
+      | _ -> false in
+
+    match e with
+    | Lambda (k, Lambda (kf, App (Var k', e'))) when k = k' && is_value e' ->
+      begin match c with
+        | Var k ->
+          App (Var k, e')
+        | Lambda (kx, kbody) ->
+          subst Ident.Map.(add kx e' empty) kbody
+        | _ -> raise (Invalid_argument "cont")
+      end
+
+    | _ ->
+      App (App (e, c), cf)
+  in
+
+  match e with
+  | Var _ | Atom _ ->
+    Lambda (k, Lambda (kf, App (Var k, e)))
+  | Prim (n, p) ->
+    let p' =
+      Prim (n + 2, fun l ->
+        match List.rev l with
+        | _ :: (Closure (_, k)) :: args ->
+          App (k, p (List.rev args))
+        | _ -> assert false)
+    in
+    Lambda (k, Lambda (kf, App (Var k, p')))
+
+  | Lambda (x, e) ->
+    Lambda (k, Lambda (kf, App (Var k, Lambda (x, cps e))))
+  | App (u, v) ->
+    let val_u = Ident.create "v" in
+    let val_v = Ident.create "v" in
+    Lambda (k,
+      Lambda (kf, 
+        cont (cps u) (Lambda (val_u,
+          cont (cps v) (Lambda (val_v,
+            cont (App (Var val_u, Var val_v)) (Var k) (Var kf))) (Var kf))) (Var kf)))
+
+  | Perform e ->
+    let val_e = Ident.create "e" in
+    lam [k; kf] (
+      cont (cps e)
+        (lam [val_e]
+           (App (App (Var kf, Var val_e), Var k))
+        ) (Var kf)
+    )
+
+  | Handle (e, hf) ->
+    let val_hf = Ident.create "hf" in
+    let x = Ident.create "x" in
+    lam [k; kf] (
+      cont (cps hf)
+        (lam [val_hf]
+           (cont
+              (cps (cont (cps e) (Lambda (x, Var x)) (Var val_hf)))
+              (Var k) (Var kf))
+        ) (Var kf)
+    )
+
+let unhandled_effect =
+  Prim
+    (1, function
+       | [Closure (_, e)] ->
+         Printf.printf "Unhandled effect: %a\n%!" print_t e; Atom Unit
+       | _ -> raise (Invalid_argument "unhandled_effect"))
+
+let cps_main e =
+  let x = Ident.create "x" in
+  let kv = Ident.create "kv" in
+  app (cps e) [lam [x] (Var x);
+               lam [x; kv] (App (unhandled_effect, Var x))]
+
+(* Interpreter *)
 
 let rec eval env = function
   | Var v ->
     (try Ident.Map.find v env with
-       Not_found -> raise Segfault)
+       Not_found -> failwith "Unbound identifier")
   | Lambda (_, _)
   | Atom _ as e ->
     Closure (env, e)
@@ -166,6 +210,7 @@ let rec eval env = function
     Closure (env, e)
   | App (u, v) ->
     apply (eval env u) (eval env v)
+  | _ -> failwith "not handled"
 
 and apply (Closure (envu, u)) (Closure (envv, v) as cv) =
   match u with
@@ -175,23 +220,39 @@ and apply (Closure (envu, u)) (Closure (envv, v) as cv) =
       eval envu (p [cv])
     else
       Closure (envu, Prim (n - 1, fun l -> p (cv :: l)))
-  | _ -> raise Segfault
+  | _ ->
+    Printf.eprintf "DEBUG: %a\n" print_t u;
+    failwith "trying to apply a value that is not a function"
 
 (** *)
 
 let ev t =
-  Printf.printf "%a\n>> " print_t t;
-  eval Ident.Map.empty t |> ignore;
-  Printf.printf "\n%!"
+  Printf.printf "%a\n" print_t t;
+  let Closure (_, res) = eval Ident.Map.empty t in
+  Printf.printf "\n>> %a\n%!" print_t res
 
-let _ =
+let ex0 =
   let x = Ident.create "x" in
-  ev (app printl [app (lam [x] (Var x)) [Atom (Int 3)]])
-
-let _ =
+  app (lam [x] (Var x)) [int 3]
+  
+let ex1 =
   let x = Ident.create "x" in
-  ev (cps_main (app printl [app (lam [x] (Var x)) [Atom (Int 3)]]))
+  (app printl [app (lam [x] (Var x)) [Atom (Int 3)]])
 
-let _ =
-  ev
-    (cps_main (seq [App (printl, int 3); App (printl, string "slt"); App (printl, string "toto")]))
+let ex1_1 =
+  seq [App (printl, string "a"); App (printl, string "b")]
+
+let ex1_2 =
+  let x = Ident.create "x" in
+  app (lam [x] (Var x)) [app (lam [x] (Var x)) [int 3]]
+
+let ex2 =
+  (seq [App (printl, int 3); App (printl, string "abc"); App (printl, string "def")])
+
+let ex3 =
+  let e = Ident.create "my_e" in
+  let k = Ident.create "my_k" in
+  Handle (seq [App (printl, string "abc");
+               App (printl, Perform (int 0));
+               App (printl, string "def")],
+          Lambda (e, Lambda (k, App (Var k, int 18))))
